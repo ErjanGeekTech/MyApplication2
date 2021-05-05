f08f9a79667f0385e45143a27f0fdb4ef984ad57
ErjanGeekTech <erjandyushebaev875@gmail.com>
2021-05-05 18:02:36 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.myapplication;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView rvTask;
+    private TaskAdapter adapter;
+
+    TextView textView;
+    FloatingActionButton btnOpenAddTask;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        rvTask = findViewById(R.id.rv_task);
+        btnOpenAddTask = findViewById(R.id.btn_open_add_task);
+        textView = findViewById(R.id.item_title_txt);
+        btnOpenAddTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddtTaskActivity.class);
+
+                Log.e("FFFF", "btnOpenAddTask " );
+                startActivityForResult(intent, 100);
+            }
+        });
+        rvTask.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new TaskAdapter(this, MainActivity.this);
+        rvTask.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100 && resultCode == RESULT_OK){
+            if (data != null) {
+                Toast.makeText(this, data.getStringExtra("title") , Toast.LENGTH_SHORT).show();
+                TaskModel model = new TaskModel(data.getStringExtra("title"), data.getStringExtra("description"), data.getStringExtra("date"));
+                adapter.addTask(model);
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/TaskAdapter.java
@@ -0,0 +1,74 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.TaskViewHolder> {
+
+    List<TaskModel> list = new ArrayList();
+    LayoutInflater inflater;
+    MainActivity activity;
+
+    public TaskAdapter(Context context, MainActivity activity){
+        this.inflater = LayoutInflater.from(context);
+        this.activity = activity;
+    }
+
+    public void addTask(TaskModel model){
+        list.add(model);
+        notifyDataSetChanged();
+    }
+
+    public void deleteTask(TaskModel model, int position){
+        list.remove(model);
+        notifyItemRemoved(position);
+    }
+
+    @NonNull
+    @Override
+    public TaskViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.item_task, parent, false);
+        return new TaskViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TaskViewHolder holder, int position) {
+        holder.txtTitle.setText(list.get(position).getTitle());
+        holder.txtDescription.setText(list.get(position).getDescription());
+        holder.txtDate.setText(list.get(position).getDate());
+//        holder.btnDelete.setOnClickListener(v -> deleteTask(list.get(position), position));
+//        holder.btnAbout.setOnClickListener(v -> {
+//            FragmentTransaction transaction = activity.getSupportFragmentManager().beginTransaction();
+//            transaction.replace(R.id.container_main, SecondFragment.newInstance(list.get(position).getTitle(), list.get(position).getDescription()));
+//            transaction.commit();
+//        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class TaskViewHolder extends RecyclerView.ViewHolder{
+        TextView txtTitle, txtDescription, txtDate;
+
+        public TaskViewHolder(@NonNull View itemView) {
+            super(itemView);
+            txtTitle = itemView.findViewById(R.id.item_title_txt);
+            txtDescription = itemView.findViewById(R.id.item_description_txt);
+            txtDate = itemView.findViewById(R.id.item_date_txt);
+        }
+    }
+}
